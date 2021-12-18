7906f2ad8cb506eabe54df8d3137e097f6d2e953
das-s <navud18260636@gmail.com>
2021-12-19 02:18:35 +0300

    GD-12 all animation and AI

--- Assets/Enemies/Weapon/Kunai/Scripts/Kunai.cs
+++ Assets/Enemies/Weapon/Kunai/Scripts/Kunai.cs
@@ -1,12 +1,16 @@
+using System;
+using Player.Scripts;
 using UnityEngine;
 
 namespace Enemies.Weapon.Kunai.Scripts
 {
     public class Kunai : MonoBehaviour
     {
-        private void OnCollisionEnter()
+        private void OnCollisionEnter(Collision other)
         {
             GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            if(other.gameObject.tag == "Player")
+                other.gameObject.GetComponentInChildren<PlayerBar>().SendMessage("TakeDamage", 20f);
             Destroy(gameObject, 10);
         }
     }
