51ca8e06fb16aa0fe6614b7923f84c3c970652cb
TheCopyNinja <thecopyninjadev@gmail.com>
2022-01-24 23:41:54 +0300

    GD-86 logic of boss fight is done

--- Assets/Enemies/Enemies/Boss/Scripts/Shield.cs
+++ Assets/Enemies/Enemies/Boss/Scripts/Shield.cs
@@ -1,5 +1,4 @@
 using Enemies.Scripts;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Enemies.Enemies.Boss.Scripts
@@ -8,6 +7,8 @@ namespace Enemies.Enemies.Boss.Scripts
     {
         [SerializeField] private GameObject[] enemies;
         private static readonly int LowerTheShields = Animator.StringToHash("LowerTheShields");
+        [SerializeField] private Vector3 startPoint;
+        private GameObject _player;
 
         public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
@@ -28,6 +29,10 @@ namespace Enemies.Enemies.Boss.Scripts
                 var randomZ = Random.Range(-5, 5);
                 Instantiate(enemies[enemyNumber], new Vector3(x + randomX, y, z + randomZ), Quaternion.identity);
             }
+            
+            // Move player out of shield
+            var player = GameObject.FindWithTag("Player");
+            player.transform.position = startPoint;
         }
 
         public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
